fe4ac9335f1ad2d6cb1e610cc4ce6f99c6e408e4
Vitor <vitor.trindade.guedess@gmail.com>
2026-09-03 13:38:08 -0300

    Adição de codigo no aruivo admin e inclusão do core e rest-frameworw no settings

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Article, Comment, Suggestion
 
-# Register your models here.
+# Registro de Modelos para teste da API
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'post-type', 'rating', 'created_at')
+    list_filter = ('category', 'post-type')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)} # Preenche o slug automaticamente quando você digita o título!
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author_name', 'article', 'is_approved', 'created_at')
+    list_filter = ('is_approved', 'created-at')
+    search_fields = ('author_name', 'created-at')
+
+
+@admin.register(Suggestion)
+class SuggestionAdmin(admin.ModelAdmin):
+    list_display = ('title_suggested', 'author_name', 'category', 'created_at')
+    list_filter = ('category',)
+    search_fields = ('title_suggested', 'author_name', 'reason')
+  
+
+
+
