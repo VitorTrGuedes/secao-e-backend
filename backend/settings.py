4c4915de4d57643925aa3c7ba30ae045d6dbbeef
Vitor Guedes <vitor.trindade.guedess@gmail.com>
2026-09-12 21:57:28 -0300

    feat:Criado modo de recuperação de senha

--- backend/settings.py
+++ backend/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django_otp.middleware.OTPmiddleware',
+    'django_otp.middleware.OTPMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
